623fec93f29a1c7cf369518ab906753b50241254
gabriel <gabrielsartarelli@outlook.com>
2023-11-23 09:09:38 -0300

    feat: configure the authorization service and implement security settings for login using JWT Token and security in application routes

---
+++ src/main/java/br/com/sartarelli011/loja/security/auth/AuthorizationService.java
@@ -0,0 +1,23 @@
+package br.com.sartarelli011.loja.security.auth;
+
+import br.com.sartarelli011.loja.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+
+        return this.userRepository.findByEmail(username);
+    }
+}
