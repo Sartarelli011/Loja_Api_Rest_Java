335391efdd60c2d17c0cdb1533afdc29f59d87bb
gabriel <gabrielsartarelli@outlook.com>
2023-11-23 08:53:36 -0300

    feat: initial configuration

---
+++ src/test/java/br/com/sartarelli011/loja/LojaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sartarelli011.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
